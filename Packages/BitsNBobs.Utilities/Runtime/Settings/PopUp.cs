1e8090da47ba37631ddc157e5cdbe0e2a0f74968
spsarras <psarras.st@gmail.com>
2020-12-23 18:14:23 +0000

    popup prefab for creating menus

---
+++ Packages/BitsNBobs.Utilities/Runtime/Settings/PopUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BitsNBobs.Extensions;
+using UnityEngine;
+
+namespace BitsNBobs
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class PopUp : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] protected bool IsOn = false;
+        public event Action Opening;
+        public event Action Hiding; 
+        void Awake()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            UpdateState();
+        }
+        
+        public void UpdateState()
+        {
+            if (IsOn)
+            {
+                MenuShow();
+            }
+            else
+            {
+                MenuHide();
+            }
+        }
+
+        public void MenuShow()
+        {
+            canvasGroup.TurnOn();
+            IsOn = true;
+            Opening?.Invoke();
+        }
+
+        public void MenuHide()
+        {
+            canvasGroup.TurnOff();
+            IsOn = false;
+            Hiding?.Invoke();
+        }
+
+    }
+}
--- Packages/BitsNBobs.Utilities/Runtime/Settings/ShowMenu.cs
+++ Packages/BitsNBobs.Utilities/Runtime/Settings/ShowMenu.cs
@@ -1,24 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using BitsNBobs.Extensions;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace BitsNBobs
 {
-    [RequireComponent(typeof(CanvasGroup))]
-    public class ShowMenu : MonoBehaviour
+    public class ShowMenu : PopUp
     {
-        [SerializeField] private CanvasGroup canvasGroup;
-
-        [SerializeField] private bool IsOn = false;
-        
-        void Awake()
-        {
-            canvasGroup = GetComponent<CanvasGroup>();
-            UpdateState();
-        }
-
         // Update is called once per frame
         void Update()
         {
@@ -31,29 +19,6 @@ namespace BitsNBobs
             
         }
 
-        public void UpdateState()
-        {
-            if (IsOn)
-            {
-                MenuShow();
-            }
-            else
-            {
-                MenuHide();
-            }
-        }
-
-        public void MenuShow()
-        {
-            canvasGroup.TurnOn();
-            IsOn = true;
-        }
-
-        public void MenuHide()
-        {
-            canvasGroup.TurnOff();
-            IsOn = false;
-        }
         
     }
 }
