32771cf6dd11dc4087bd195fb82d206ef830e1b1
spsarras <psarras.st@gmail.com>
2020-12-24 11:12:47 +0000

    some utility functions

---
+++ Packages/BitsNBobs.Utilities/Runtime/Extensions/GradientExtension.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace BitsNBobs.Extensions
+{
+    public static class GradientExtension
+    {
+        public static Gradient GetHeatMap()
+        {
+            return new Gradient()
+            {
+                alphaKeys = new[]
+                {
+                    new GradientAlphaKey(1, 0),
+                    new GradientAlphaKey(1, 0.25f),
+                    new GradientAlphaKey(1, 0.5f),
+                    new GradientAlphaKey(1, 0.75f),
+                    new GradientAlphaKey(1, 1),
+                },
+                colorKeys = new[]
+                {
+                    new GradientColorKey(UnityEngine.Color.blue, 0),
+                    new GradientColorKey(UnityEngine.Color.cyan, 0.25f),
+                    new GradientColorKey(UnityEngine.Color.green, 0.5f),
+                    new GradientColorKey(UnityEngine.Color.yellow, 0.75f),
+                    new GradientColorKey(UnityEngine.Color.red, 1),
+                }
+            };
+        }
+    }
+}
---
+++ Packages/BitsNBobs.Utilities/Runtime/Extensions/MeshExtension.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace BitsNBobs.Extensions
+{
+    public static class MeshExtension
+    {
+        public static Mesh Clone(this Mesh mesh)
+        {
+            var m = new Mesh();
+            m.vertices = mesh.vertices;
+            m.triangles = mesh.triangles;
+            m.colors = mesh.colors;
+            m.normals = mesh.normals;
+            m.RecalculateBounds();
+            m.RecalculateTangents();
+            return m;
+        }
+        
+    }
+}
