35cc903cfd7167c3aa0b6293ce72db407c39c238
spsarras <psarras.st@gmail.com>
2021-01-06 12:17:32 +0000

    layout extension method for not autosizing

---
+++ Packages/BitsNBobs.Utilities/Runtime/Extensions/LayoutGroupExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BitsNBobs.Extensions
+{
+    public static class LayoutGroupExtension
+    {
+        public static void EnableDisable(this LayoutGroup group, MonoBehaviour behaviour)
+        {
+            
+            behaviour.StartCoroutine(EnableDisable(group));
+        }
+
+        private static IEnumerator EnableDisable(LayoutGroup group)
+        {
+            group.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+            group.enabled = true;
+        }
+        
+    }
+}
