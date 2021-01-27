1af9011b51b4b35a321deacb2013ebba7b8666fc
spsarras <psarras.st@gmail.com>
2021-01-27 17:15:42 +0000

    Added a converter for multiple colors

--- Packages/BitsNBobs.Utilities/Runtime/Extensions/ColorExtension.cs
+++ Packages/BitsNBobs.Utilities/Runtime/Extensions/ColorExtension.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
 
 namespace BitsNBobs.Extensions
 {
@@ -7,6 +9,11 @@ namespace BitsNBobs.Extensions
         public static Color ToUnity(this System.Drawing.Color color)
         {
             return new Color(color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f);
+        }
+
+        public static IEnumerable<Color> ToUnity(this IEnumerable<System.Drawing.Color> colors)
+        {
+            return colors.Select(x => x.ToUnity());
         }
     }
 }
