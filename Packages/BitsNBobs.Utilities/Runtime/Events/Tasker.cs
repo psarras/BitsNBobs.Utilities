e0ae9016a88e6b96abfcdd4c864403e333b218ea
spsarras <psarras.st@gmail.com>
2021-02-04 16:29:28 +0000

    Implemented a helper class Tasker for refreshing data within a certain time limit

---
+++ Packages/BitsNBobs.Utilities/Runtime/Events/Tasker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace BitsNBobs.Events
+{
+    public class Tasker
+    {
+        private bool update = false;
+        public float repeatRate { get; set; } = 0.1f;
+        public Action AnyAction { get; set; }
+
+        public void Update()
+        {
+            update = true;
+        }
+
+        public Coroutine StartWatching(MonoBehaviour handler)
+        {
+            return handler.StartCoroutine(UpdateRoutine());
+        }
+
+        public IEnumerator UpdateRoutine()
+        {
+            while (true)
+            {
+                if (!update)
+                {
+                    yield return new WaitForSeconds(repeatRate);
+                    continue;
+                }
+
+                AnyAction();
+                update = false;
+                yield return new WaitForSeconds(repeatRate);
+            }
+        }
+    }
+}
