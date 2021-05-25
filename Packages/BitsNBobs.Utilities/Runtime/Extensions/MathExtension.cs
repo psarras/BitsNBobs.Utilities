e46853cef6a5c9f9c2c7720ba97ebba0ffdc6510
spsarras <psarras.st@gmail.com>
2021-05-25 12:36:19 +0100

    added manhatan distance calc

---
+++ Packages/BitsNBobs.Utilities/Runtime/Extensions/MathExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BitsNBobs.Extensions
+{
+    public static class MathExtension
+    {
+        public static float ManhattanDist(float[] a, float[] b)
+        {
+            if (a.Length != b.Length)
+                throw new Exception("Vectors need to be of the same length");
+            var d = 0f;
+            
+            for (int i = 0; i < b.Length; i++)
+            {
+                d += b[i] - a[i];
+            }
+
+            return Math.Abs(d);
+        }
+    }
+}
