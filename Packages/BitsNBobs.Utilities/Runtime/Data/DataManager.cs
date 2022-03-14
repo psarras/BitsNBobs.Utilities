b8b8654e7509b1f989a8e07876cd059516689901
Stamatios Psarras <psarras.st@gmail.com>
2022-03-14 10:17:04 +0000

    Added some IO utilities and base classes (#2)

---
+++ Packages/BitsNBobs.Utilities/Runtime/Data/DataManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+namespace BitsNBobs.Data
+{
+    public abstract class DataManager<T> : MonoBehaviour where T : new ()
+    {
+        [SerializeField] private string saveFilename;
+        public T UserData { get; private set; }
+        public Action OnDataUpdate;
+        public Action OnDataLoaded;
+        
+        private void Start()
+        {
+            LoadOrCreate();
+        }
+
+        private void LoadOrCreate()
+        {
+            UserData = DataIo.LoadOrCreateUnity<T>(saveFilename); //"data.gd"
+            OnDataLoaded?.Invoke();
+        }
+
+        protected void Save()
+        {
+            DataIo.SaveDataUnity(saveFilename, UserData);
+        }
+
+        public void OnSave()
+        {
+            Save();
+            Debug.Log("OnSave");
+            OnDataUpdate?.Invoke();
+        }
+    }
+}
---
+++ Packages/BitsNBobs.Utilities/Runtime/Manage/ListManagerUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BitsNBobs.Manage
+{
+    public static class ListManagerUtilities
+    {
+        public static ListManager<T> ToListManager<T>(this List<T> list)
+        {
+            ListManager<T> d = new ListManager<T>();
+            foreach (var l in list)
+            {
+                d.Add(l);
+            }
+            return d;
+        }
+    }
+}
