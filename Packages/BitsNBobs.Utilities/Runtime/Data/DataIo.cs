308b160ecf2aff37cb15aa7ec84cfc43dd81724a
spsarras <psarras.st@gmail.com>
2022-03-14 10:16:43 +0000

    Added some IO utilities and base classes

---
+++ Packages/BitsNBobs.Utilities/Runtime/Data/DataIo.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using Unity.Plastic.Newtonsoft.Json;
+using UnityEngine;
+
+namespace BitsNBobs.Data
+{
+    public static class DataIo
+    {
+        public static T LoadOrCreate<T>(string directory, string file) where T : new()
+        {
+            var path = Path.Combine(directory, file);
+            if (File.Exists(path))
+                return LoadData<T>(directory, file);
+            var t = new T();
+            SaveData(directory, file, t);
+            return t;
+        }
+
+        public static T LoadOrCreateUnity<T>(string file) where T : new()
+        {
+            return LoadOrCreate<T>(Application.persistentDataPath, file);
+        }
+
+        public static void SaveDataUnity<T>(string file, T data)
+        {
+            SaveData<T>(Application.persistentDataPath, file, data);
+        }
+
+        public static T LoadData<T>(string directory, string file) where T : new()
+        {
+            var path = Path.Combine(directory, file);
+            var data = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        public static void SaveData<T>(string directory, string file, T data)
+        {
+            var txt = JsonConvert.SerializeObject(data, Formatting.Indented);
+            var path = Path.Combine(directory, file);
+            File.WriteAllText(path, txt);
+        }
+    }
+}
---
+++ Packages/BitsNBobs.Utilities/Runtime/Manage/ListManagerExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace BitsNBobs.Manage
+{
+    public static class ListManagerExtensions
+    {
+        public static bool Next<T>(this ListManager<T> listManager, out T value)
+        {
+            value = default;
+            if (listManager.IsMax()) 
+                return false;
+            listManager.Next();
+            value = listManager.Get();
+            return true;
+        }
+        
+        public static bool Back<T>(this ListManager<T> listManager, out T value)
+        {
+            value = default;
+            if (listManager.IsMin()) 
+                return false;
+            listManager.Back();
+            value = listManager.Get();
+            return true;
+        }
+
+        public static bool IsMax<T>(this ListManager<T> listManager)
+        {
+            return listManager.Index >= listManager.Count - 1;
+        }
+        
+        public static bool IsMin<T>(this ListManager<T> listManager)
+        {
+            return listManager.Index == 0;
+        }
+    }
+}
